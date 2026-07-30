013eb37de0ba1ecebd7f5ba1e8434bcea37404b5
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-30 10:06:35 +0530

    fix: configure trust proxy for correct rate limiting behind Render's reverse proxy

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ const errorHandler = require('./middleware/errorHandler');
 const AppError = require('./utils/AppError');
 
 const app = express();
+app.set('trust proxy', 1);
 
 app.use(helmet());
 app.use(cors());
